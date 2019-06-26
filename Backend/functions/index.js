ca78247f88f3895735d321aba095e08338b39b81
Marcvs101 <marcocuoci@yahoo.it>
2019-06-26 16:51:54 +0200

    Aggiunta autenticazione backend

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,8 +1,45 @@
+//Connect to DB
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+let db = admin.firestore();
+
+/*
+//Read data
+db.collection('utenti').get()
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data());
+        });
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+    });
+*/
+exports.RegisterNewUser = functions.auth.user().onCreate((user) => {
+    const email = user.email; // The email of the user.
+    const displayName = user.displayName; // The display name of the user.
+    const uid = user.uid;
+
+    //Add data
+    let docRef = db.collection('utenti').doc(uid);
+    let seUser = docRef.set({
+        'nome': displayName,
+        'email': email
+    });
+
+    console.log("User " + string(uid) + " aka " + string(displayName) + " joined the world");
+});
+
+exports.UnregisterUser = functions.auth.user().onDelete((user) => {
+    const email = user.email; // The email of the user.
+    const displayName = user.displayName; // The display name of the user.
+    const uid = user.uid;
+
+    //Remove entry from DB
+    let docRef = db.collection('utenti').doc(uid).delete();
+
+    console.log("User " + string(uid) + " aka " + string(displayName) + " left the world");
+});
