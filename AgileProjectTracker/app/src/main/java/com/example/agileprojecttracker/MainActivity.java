6d4c265dac6f0c08b62d809b0f8185d3719885cb
LeonardoPetrucci <leonardo.petrucci94@gmail.com>
2019-06-27 13:38:30 +0200

    start login

--- AgileProjectTracker/app/src/main/java/com/example/agileprojecttracker/MainActivity.java
+++ AgileProjectTracker/app/src/main/java/com/example/agileprojecttracker/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.agileprojecttracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GithubAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent signIn = new Intent(this, SigninActivity.class);
+        startActivity(signIn);
     }
+
+
 }
