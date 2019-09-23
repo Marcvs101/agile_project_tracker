eec98947c11b8ad0ad2810cc801c9c5dc8ac014f
Salvatore <salvolicitra95@gmail.com>
2019-09-23 19:31:45 +0200

    minor

--- apt/lib/model/project.dart
+++ apt/lib/model/project.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'developer.dart';
-
-import 'package:cloud_functions/cloud_functions.dart';
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
@@ -19,31 +16,6 @@ class Project {
 
   IconData _icona;
   Color _c;
-
-  static List pp = [
-    Project(
-      "Progetto numero 1", 
-      "salvo", 
-      "questo è il progetto numero 1, bla bla bla...\n aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbcccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbaaaaaaa\n\n\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaapapapapapa",
-      false,
-      "1111",
-    ),
-    Project(
-      "progetto 2",
-      "leonardo",
-      "questo è il progetto numero 2, bla bla bla...",
-      true,
-      "2222"
-    ),
-    Project(
-      "progetto 3",
-      "marco",
-      "questo è il progetto numero 3, bla bla bla...",
-      false,
-      "3333"
-    )
-  ];
-
 
   Project(String n, String p, String d, bool c, this.id){
     this.nome = n;
@@ -69,43 +41,19 @@ class Project {
     return this._c;
   }
   
- /* 
-  //prendo in input l'id dell'utente
-  static List<Project> getProjects(String uid) {
-    /* 
-     * Query da implementare:
-     * la chiamata proviene dal costruttore in homepage
-     * dovrebbe prendere in input l'id dell'utente -> viene passato tramite user.uid
-     * e ritornare una List dei progetti cui l'utente
-     * ha collaborato, non solo quelli in cui
-     * è il proprietario
-     */
-    print(uid);
-    var a = query(uid);
-    a.then((val){
-      return val;
-    });
-    //List<Project> res = Future..result(a, secondsToWait 1);
-
-    //var future = new Future(query(uid).then(value){return value;}
-    //return l;
-  }*/
-
   static Future<List> getProjects(String uid) async {
-    /* 
-     * Query da implementare:
-     * la chiamata proviene dal costruttore in homepage
-     * dovrebbe prendere in input l'id dell'utente -> viene passato tramite user.uid
-     * e ritornare una List dei progetti cui l'utente
-     * ha collaborato, non solo quelli in cui
-     * è il proprietario
+ 
+    /* Query:
+     * la chiamata prende in input l'id dell'utente tramite user.uid
+     * e ritorna una List dei progetti cui l'utente ha collaborato,
+     * non solo quelli di cui ne è il proprietario
      */
     var url = 'https://us-central1-agile-project-tracker.cloudfunctions.net/GetProjectsForUser'+'/'+uid;
-    var response = await http.get(url);//, body: {uid});
+    var response = await http.get(url);
     
-    //http.post(url, body);
-    print('Response status: ${response.statusCode}');
-    print('Response parsed:${json.decode(response.body)}');
+    //Stampe utili per il debug:
+    //print('Response status: ${response.statusCode}');
+    //print('Response parsed:${json.decode(response.body)}');
     
     List<Project> projects = [];
     Map<String, dynamic> projectsMap = json.decode(response.body);
