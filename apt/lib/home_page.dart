5ea43debf6900baa8b9a632f1f8c30e4c0efcba7
Salvatore <salvolicitra95@gmail.com>
2019-10-02 11:30:51 +0200

    create project

--- apt/lib/home_page.dart
+++ apt/lib/home_page.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase/firestore.dart';
 import 'package:flutter/material.dart';
 import 'model/project.dart';
 import 'project_page.dart';
+import 'package:async/async.dart';
 
 
 class HomePage extends StatefulWidget {
@@ -85,12 +89,99 @@ class _HomePageState extends State<HomePage>{
       title: Text(widget.user.displayName),
     );
 
-    return Scaffold(
-      appBar: topAppBar,
-      backgroundColor: Colors.grey,
-      body: new FutureBuilder(
+    Future<Null> _showAddUserDialogBox(BuildContext context) {
+      TextEditingController _nameTextController = new TextEditingController(); 
+      TextEditingController _descrTextController = new TextEditingController();
+
+      return showDialog<Null>(
+        context: context,
+        builder: (BuildContext context) {
+          return new AlertDialog(
+            title: const Text("Add a project"),
+            content: Container(
+              height: 200.0,
+              width: 140.0,
+              child: ListView(
+                children: <Widget>[
+                  new TextField(
+                    controller: _nameTextController,
+                    decoration: const InputDecoration(labelText: "Name: "),
+                  ),
+                  new TextField(
+                    controller: _descrTextController,
+                    decoration: const InputDecoration(labelText: "Description: "),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                  ),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Cancel")
+              ),
+              // This button results in adding the contact to the database
+              new FlatButton(
+                  onPressed: () {
+//------------------NEW CODE---------------------------------------------
+                    CloudFunctions.instance.call(
+                      functionName: "CreateNewProject",
+                      parameters: {
+                        "nome": _nameTextController.text,
+		                    "descrizione": _descrTextController.text,
+                        "proprietario": widget.user.uid,
+                        "user_story": [],
+                        "completato": false,
+		                    "sviluppatori": [],
+		                    "amministratori": [widget.user.uid],
+		                  }
+                    );
+// ------------END OF NEW CODE-------------------------------------------
+                    Navigator.of(context).pop();
+                },
+                child: const Text("Confirm")
+              )
+            ],
+          );
+        }
+      );
+    }
+
+    
+    StreamBuilder<QuerySnapshot> _retrieveProject() {
+      return new StreamBuilder<QuerySnapshot>(
+      // Interacts with Firestore (not CloudFunction)
+      //stream: new CollectionReference collection('progetti').snapshots(),
+      stream: Firestore.instance.collection('progetti').where('sviluppatori', arrayContains: widget.user.uid ).snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData || snapshot.data == null) {
+            return Container();
+          }
+          return ListView.builder(
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (context, index) {
+              var v = snapshot.data.documents[index];
+              Project p = new Project(v['nome'],v['proprietario'],v['descrizione'],v['completato'],v['uid']);
+              return makeCard(p);
+              //return new ListTile(
+              //  title: new Text(snapshot.data.documents[index]['name']),
+              //  subtitle: new Text(snapshot.data.documents[index]['email'])
+              //);
+            }
+          );
+        }
+      );
+    }
+
+    FutureBuilder _loadProject(){
+      return new FutureBuilder<List>(
         future: Project.getProjects(widget.user.uid),
-        builder: (BuildContext context, AsyncSnapshot<List> snapshot){
+        builder: (context, AsyncSnapshot<List> snapshot) {
           if (!snapshot.hasData)
             return new Container();
           var content = snapshot.data;
@@ -107,8 +198,23 @@ class _HomePageState extends State<HomePage>{
             }
           );
         }
+        
+      );
+    }
 
-      )
+    return Scaffold(
+      appBar: topAppBar,
+      backgroundColor: Colors.grey,
+      //body: _retrieveProject(),
+      
+      body: _loadProject(),
+     
+      floatingActionButton: new FloatingActionButton(
+        backgroundColor: Color.fromRGBO(58, 66, 86, 0.9),
+        onPressed: () => _showAddUserDialogBox(context),
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ),
     );
   }
 }
